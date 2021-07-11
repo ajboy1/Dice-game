2045fa1c9d3e65e4ed9b931d097a5744bb50f9f9
ajboy1 <ajboy19011996@gmail.com>
2021-07-11 17:21:05 +0530

    all updated

---
+++ index.js
@@ -0,0 +1,34 @@
+// For Image1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var diceImageSrc1 = "/images/" + randomDiceImage;
+
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", diceImageSrc1);
+
+// For Image 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber2 + ".png";
+
+var diceImageSrc2 = "/images/" + randomDiceImage;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", diceImageSrc2);
+
+
+// Change h1 Content
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🎈🎉Player 1 WIN!"
+} else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 WIN!🎇🎆"
+} else{
+    document.querySelector("h1").innerHTML = "🤣Draw Game!!😂"
+}
+
